93836f777751c4d4ecc169059488db54c0bb7642
camilapanella <ca10_mimi@hotmail.com>
2023-02-12 14:27:05 -0300

    upload controller done

--- src/controller.js
+++ src/controller.js
@@ -1,27 +1,40 @@
-const Records = require('./records.model');
+const Records = require("./records.model");
+const csv = require("csvtojson");
+const fs = require("fs");
 
 const upload = async (req, res) => {
-    const {file} = req;
+  // console.log(req)
+  const { file } = req;
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
+  /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
 
-    return res.status(200).json({ message: 'some response' });
+  csv()
+    .fromFile(file.path)
+    .then((data) => {
+      Records.insertMany(data)
+        .then(function () {
+          fs.unlink(file.path, (err) => {
+            if (err) return res.status(500).json(err);
+          });
+          res.status(200).json({ message: "Data successfully imported" });
+        })
+        .catch(function (err) {
+          return res.status(500).json(err);
+        });
+    });
 };
 
 const list = async (_, res) => {
-    try {
-        const data = await Records
-            .find({})
-            .limit(10)
-            .lean();
-        
-        return res.status(200).json(data);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+  try {
+    const data = await Records.find({}).limit(10).lean();
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 module.exports = {
-    upload,
-    list,
+  upload,
+  list,
 };
